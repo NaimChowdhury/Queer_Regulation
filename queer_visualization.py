1208b414c6c28f4643b8ae56e7ebe93c6bc7e589
NaimChowdhury <naimul.chowdhury15@ncf.edu>
2024-03-25 16:56:40 +0800

    Reframe as a plotnine problem.

--- queer_visualization.py
+++ queer_visualization.py
@@ -1,14 +1,11 @@
 import pandas as pandas
+from plotnine import *
 
-import plotly.graph_objects as go
 
 # Data
-events = ['罪化与病态化的中国', '非罪化与半去病化的中国', '中国新时代的监管']
-start_years = [1958, 2002, 2015]
-end years = [2001, 2014, 2024]
+data = {
+	'Event' : ['罪化与病态化的中国', '罪化与病态化的中国', '非罪化与半去病化的中国', '中国新时代的监管']
+	'Start Year' : [1958, 2002, 2015],
+	'End Year' : [2001, 2014, 2024]
+}
 
-# Calculate duration of each event
-durations = [end - start for start, end in zip(start_years, end_years)]
-
-# Instantiate a plotly figure
-fig = go.Figure()
