aa8c7490ba1ddc80f4cc7a790dfa10e6b231a813
NaimChowdhury <naimul.chowdhury15@ncf.edu>
2024-03-25 15:14:29 +0800

    Create dummy data.

--- queer_visualization.py
+++ queer_visualization.py
@@ -1,1 +1,9 @@
 import pandas as pandas
+
+import plotly.graph_objects as go
+
+# Data
+events = ['罪化与病态化的中国', '非罪化与半去病化的中国', '中国新时代的监管']
+start_years = [1958, 2002, 2015]
+end years1 = [2001, 2014, 2024]
+
